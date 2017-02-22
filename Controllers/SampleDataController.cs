abc5a2080a53557c1ebf8514444124231ba1af35
Jonatan Lundin <jonatan.lundin@gmail.com>
2017-02-22 11:29:05 +0100

    Add sample data

--- Controllers/SampleDataController.cs
+++ Controllers/SampleDataController.cs
@@ -1,44 +1,48 @@
 using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Dynamic;
 using Microsoft.AspNetCore.Mvc;
 
-namespace GearStore.Controllers
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Test.Controllers
 {
     [Route("api/[controller]")]
     public class SampleDataController : Controller
     {
-        private static string[] Summaries = new[]
+        [HttpGet("[action]"), Produces("application/json")]
+        public string Categories()
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        [HttpGet("[action]")]
-        public IEnumerable<WeatherForecast> WeatherForecasts(int startDateIndex)
-        {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                DateFormatted = DateTime.Now.AddDays(index + startDateIndex).ToString("d"),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            });
+            return System.IO.File.ReadAllText(@"./Controllers/data/categories.json");
         }
 
-        public class WeatherForecast
+        [HttpGet("[action]")]
+        public JsonResult Products(string category)
         {
-            public string DateFormatted { get; set; }
-            public int TemperatureC { get; set; }
-            public string Summary { get; set; }
+            string productsJson = System.IO.File.ReadAllText(@"./Controllers/data/products.json");
+            string imagesJson = System.IO.File.ReadAllText(@"./Controllers/data/images.json");
 
-            public int TemperatureF
-            {
-                get
-                {
-                    return 32 + (int)(TemperatureC / 0.5556);
-                }
+            var expConverter = new ExpandoObjectConverter();
+            var products = JsonConvert.DeserializeObject<List<ExpandoObject>>(productsJson, expConverter);
+            var images = JsonConvert.DeserializeObject<Dictionary<string, string[]>>(imagesJson);
+
+            if (category != null) {
+                products = products.FindAll(prd => {
+                    return ((prd as dynamic).category).Contains(category);
+                });
             }
+
+            return Json(products.Select<dynamic, dynamic>(prd => {
+                string[] imgs;
+
+                images.TryGetValue(prd.sku, out imgs);
+                prd.images = imgs.Select(img => "/images/product" + img);
+
+                return prd;
+            }));
         }
     }
 }
