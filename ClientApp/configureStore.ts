d14a4d663983a5f9a8bc12912c9b59f5edb42313
Jonatan Lundin <jonatan.lundin@gmail.com>
2017-02-23 13:18:06 +0100

    Persist cart items

--- ClientApp/configureStore.ts
+++ ClientApp/configureStore.ts
@@ -3,6 +3,9 @@ import thunk from 'redux-thunk';
 import { routerReducer } from 'react-router-redux';
 import * as Store from './store';
 
+import * as Products from './store/Products';
+import * as Cart from './store/Cart';
+
 export default function configureStore(initialState?: Store.ApplicationState) {
     // Build middleware. These are functions that can process the actions before they reach the store.
     const windowIfDefined = typeof window === 'undefined' ? null : window as any;
@@ -23,6 +26,29 @@ export default function configureStore(initialState?: Store.ApplicationState) {
             const nextRootReducer = require<typeof Store>('./store');
             store.replaceReducer(buildRootReducer(nextRootReducer.reducers));
         });
+    }
+
+    if (windowIfDefined && windowIfDefined.localStorage) {
+        // Persist cart items on client side
+        let lastState = null
+        store.subscribe(() => {
+            const state = store.getState()
+            if (!lastState) {
+                try {
+                    const items = JSON.parse(windowIfDefined.localStorage.getItem('cart'))
+                    if (items.length) {
+                        store.dispatch(Products.actionCreators.requestProducts(null))
+                        store.dispatch(Cart.actionCreators.restoreCart(items))
+                        lastState = items
+                    } else {
+                        lastState = []
+                    }
+                } catch (e) {}
+            } else if (state.cart.items !== lastState) {
+                lastState = state.cart.items
+                windowIfDefined.localStorage.setItem('cart', JSON.stringify(state.cart.items))
+            }
+        })
     }
 
     return store;
